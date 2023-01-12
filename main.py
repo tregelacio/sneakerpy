e577b52f7fe0b1e80f71409ed195bdc589c4a067
tregelacio <tre.gelacio@gmail.com>
2023-01-11 22:12:33 -1000

    Set up basic requests for goat.com
    import requests for web scraping and json to store data into a json file. Headers are used because website doesn't load for requests that don't have one

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+def search(query):
+    url = f'https://www.goat.com/search?query={query}'
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'dnt': '1',
+        'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
+    html = requests.get(url=url, headers=headers)
+    output = json.loads(html.text)
+    return output['Products'][0]
+
+search('dunk')
